b338f76995c9685f38b744fd5d10ba82ce4ddbee
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-04-30 02:08:38 -0500

    feat: add route for product editing page

--- reto_angular/src/app/app.routes.ts
+++ reto_angular/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { ProductsPage } from './pages/products-page/products-page';
 import { ProductFormPage } from './pages/product-form-page/product-form-page';
+import { ProductEditPage } from './pages/product-edit-page/product-edit-page';
 
 export const routes: Routes = [
   {
     path: 'products/new',
     component: ProductFormPage,
+  },
+  {
+    path: 'products/update/:productId',
+    component: ProductEditPage,
   },
   {
     path: 'products',
