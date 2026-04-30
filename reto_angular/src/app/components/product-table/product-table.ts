cf2406b4eba24925f17cdfd1c65d935cee695dff
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-04-30 02:08:34 -0500

    feat: add context menu for product actions in product table

--- reto_angular/src/app/components/product-table/product-table.ts
+++ reto_angular/src/app/components/product-table/product-table.ts
@@ -1,13 +1,23 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { Product } from '@core/product.model';
+import { ContextMenuComponent, MenuItem } from '@components/context-menu/context-menu';
 
 @Component({
   selector: 'app-product-table',
-  imports: [DatePipe],
+  imports: [DatePipe, ContextMenuComponent],
   templateUrl: './product-table.html',
   styleUrl: './product-table.css',
 })
 export class ProductTableComponent {
   readonly products = input<Product[]>([]);
+  readonly editProduct = output<string>();
+
+  readonly menuItems: MenuItem[] = [{ label: 'Editar Producto', action: 'edit' }];
+
+  onMenuAction(item: MenuItem, productId: string): void {
+    if (item.action === 'edit') {
+      this.editProduct.emit(productId);
+    }
+  }
 }
